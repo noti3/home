a1130b4435be4057ca73755392a9eeca582c7d29
tuphan-dn <tuphan@descartes.network>
2023-11-17 11:21:51 +0700

    fix: uifix

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,7 +2,7 @@ import { LiteLogo } from '@/components/noti3'
 
 export default function Footer() {
   return (
-    <div className="w-full flex flex-auto gap-2 p-4 items-center border-t-2 border-base-300">
+    <div className="w-full flex flex-auto gap-2 p-4 items-center border-t-2 border-base-200">
       <LiteLogo className="h-4 text-black dark:text-white" />
       <p className="text-sm">noti3 © 2023, All Rights Reserved.</p>
       <div className="flex-auto" />
