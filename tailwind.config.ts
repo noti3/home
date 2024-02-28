ffc7c58a0020b5493540928c06257d3ba5c67041
tuphan-dn <tuphan@descartes.network>
2024-02-28 15:13:11 +0700

    fix: add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import daisyui from 'daisyui/src/theming/themes'
 
 const config: Config = {
   darkMode: ['class', '[data-theme="dark"]'],
@@ -11,6 +12,28 @@ const config: Config = {
     extend: {},
   },
   plugins: [require('@tailwindcss/container-queries'), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...daisyui['light'],
+          primary: '#ff6c61',
+          'primary-content': '#4e4351',
+          secondary: '#ff6c61',
+          'secondary-content': '#dbd2d1',
+        },
+      },
+      {
+        dark: {
+          ...daisyui['dark'],
+          primary: '#ff6c61',
+          'primary-content': '#dbd2d1',
+          secondary: '#ff6c61',
+          'secondary-content': '#4e4351',
+        },
+      },
+    ],
+  },
 }
 
 export default config
